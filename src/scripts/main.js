eac87f4b4a2dd510b91c68d045327bbeb1895eea
Logan Demmy <frank.demmy@gmail.com>
2021-03-05 15:16:04 -0600

    added a dynamic dad joke

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
-import { getUsers, getPosts } from "../data/DataManager.js"
+import { getDadJoke, getPosts } from "../data/DataManager.js"
 import { PostList } from "./feed/postlist.js"
-
+import { Joke } from "./dadjoke/joke.js"
 
 const showPostList = () => {
     const postElement = document.querySelector(".postList");
@@ -9,10 +9,29 @@ const showPostList = () => {
       })
 }
 
+const showDadJoke = () => {
+    const postElement = document.querySelector(".dadjoke");
+      getDadJoke().then((joke) => {
+          postElement.innerHTML = Joke(joke);
+      })
+}
 const startGiffyGram = () => {
 	showPostList();
 }
 
+const dadjokes = () => {
+    showDadJoke();
+}
+
+dadjokes();
+
+
+document.getElementById("newjoke").onclick = function() {dadjokes()}
 // Are you defining the function here or invoking it?
 startGiffyGram();
 
+// applicationElement.addEventListener("click", event => {
+// 	if (event.target.id === "newdadjoke"){
+// 		newdadjoke();
+// 	}
+// })
