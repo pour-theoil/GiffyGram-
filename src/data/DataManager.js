3866914ffc5ac53f604b157063883b7b1f1cff4d
Logan Demmy <frank.demmy@gmail.com>
2021-03-10 10:46:14 -0600

    added a filter method when the year is changed

--- src/data/DataManager.js
+++ src/data/DataManager.js
@@ -9,16 +9,6 @@ export const getUsers = () => {
     // })
 }
 
-export const getPosts = () => {
-
-    return fetch("http://localhost:8088/posts")
-    .then(response => response.json())
-    // .then(parsedResponse => {
-    //     // do something with response here
-    //     return parsedResponse;
-    // })
-}
-
 export const getDadJoke = () => {
 
     return fetch('https://icanhazdadjoke.com/', {
@@ -32,6 +22,23 @@ export const getDadJoke = () => {
     //     // do something with response here
     //     return parsedResponse;
     // })
+}
+
+let postCollection = [];
+
+export const usePostCollection = () => {
+  //Best practice: we don't want to alter the original state, so
+  //make a copy of it and then return it
+  //The spread operator makes this quick work
+  return [...postCollection];
+}
+export const getPosts = () => {
+  return fetch("http://localhost:8088/posts")
+    .then(response => response.json())
+    .then(parsedResponse => {
+      postCollection = parsedResponse
+      return parsedResponse;
+    })
 }
 
 export const createPost = postObj => {
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-import { getDadJoke, getPosts, createPost } from "../data/DataManager.js"
+import { getDadJoke, getPosts, createPost, usePostCollection } from "../data/DataManager.js"
 import { PostList } from "./feed/postlist.js"
 import { Joke } from "./dadjoke/joke.js"
 import { NavBar } from "./nav/NavBar.js"
@@ -39,12 +39,26 @@ const startGiffyGram = () => {
 	showPostList();
 }
 
-document.getElementById("newjoke").onclick = function() {dadjokes()}
+const showFilteredPosts = (year) => {
+    //get a copy of the post collection
+    const epoch = Date.parse(`01/01/${year}`);
+    //filter the data
+    const filteredData = usePostCollection().filter(singlePost => {
+        if (singlePost.timestamp >= epoch) {
+            return singlePost
+        }
+    })
+    const postElement = document.querySelector(".postList");
+    postElement.innerHTML = PostList(filteredData);
+}
+
 
 
 // eventlistener section, we have created the variable applicationElement that 
 // will "listen" for diffent events happening in the .giffygram part of the page.
 const applicationElement = document.querySelector(".giffygram");
+
+document.getElementById("newjoke").onclick = function() {dadjokes()}
 
 applicationElement.addEventListener("click", event => {
 	if (event.target.id === "logout"){
@@ -58,6 +72,7 @@ applicationElement.addEventListener("click", event => {
     }
   })
 
+  // Change Event for year
 applicationElement.addEventListener("change", event => {
     if (event.target.id === "yearSelection") {
       const yearAsNumber = parseInt(event.target.value)
@@ -65,6 +80,7 @@ applicationElement.addEventListener("change", event => {
     }
 })
 
+// Ability to add new posts
 applicationElement.addEventListener("click", event => {
     event.preventDefault();
     if (event.target.id === "newPost__submit") {
@@ -89,6 +105,7 @@ applicationElement.addEventListener("click", event => {
     
   })
 
+// Log if which edit button is clicked
 applicationElement.addEventListener("click", (event) => {
 	
 	if (event.target.id.startsWith("edit")){
@@ -97,6 +114,17 @@ applicationElement.addEventListener("click", (event) => {
 	}
 })
 
+// Log which year has been requested
+applicationElement.addEventListener("change", event => {
+    if (event.target.id === "yearSelection") {
+      const yearAsNumber = parseInt(event.target.value)
+      console.log(`User wants to see posts since ${yearAsNumber}`)
+      //invoke a filter function passing the year as an argument
+      showFilteredPosts(yearAsNumber);
+    }
+})
+
+// Execution functions
 showNavBar();
 dadjokes();
 showFooter();
@@ -109,3 +137,5 @@ const showPostEntry = () => {
     
 }
 showPostEntry();
+
+
