fffbbcad466f73890c4f155d1677fde86a120e45
Logan Demmy <frank.demmy@gmail.com>
2021-03-23 09:24:11 -0500

    Delete button added to the post

--- src/scripts/feed/post.js
+++ src/scripts/feed/post.js
@@ -6,6 +6,7 @@ export const Post = (postObject) => {
         </header>
         <img class="post__image" src="${postObject.imageURL}" />
         <div><button id="edit--${postObject.id}">Edit</button></div>
+        <button id="delete__${postObject.id}">Delete</button>
       </section>
     `
   }
